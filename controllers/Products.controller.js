379a6fbf107c65a19cc53627012c61613b449d6c
Divy Motisariya <motisariyadivy2@gmail.com>
2021-07-15 17:15:17 +0530

    Added Products report and PDF generation using : npm html-pdf

--- controllers/Products.controller.js
+++ controllers/Products.controller.js
@@ -6,13 +6,41 @@ const {
 const connection = require("../config/db.config");
 const { showQuery, dataQuery } = require("../helpers/connectionQueries");
 
+const pug = require('pug');
+const path = require('path');
+const pdf = require('html-pdf')
+const fs = require('fs');
+
 const showProducts = (req, res) => {
   const sql = `select p.id, p.name, p.hsncode, p.description, p.stock, p.is_active, ps.id 'Stock ID', ps.product_stock
     from products p
     left join productstock ps on ps.id = p.id;`;
   // where ps.product_stock > 0;`;
-  showQuery(res, sql, "Product List");
-};
+  // showQuery(res, sql, "Product List");
+  connection.query(sql, (err, rows) => {
+    if (err) return errorRes(res, err);
+    // rows.forEach(product => product.image = path.join("file:///", __dirname, "../public/images", "/test.png").replace(/\//g, '\\'))
+    rows.forEach(product => product.image = path.join("/images", "/test.png"))
+      // console.log(rows);
+    pug.renderFile(path.resolve(__dirname + '/../views/productList.pug').replace(/\\/g, '/'), { products: rows }, (err, html) => {
+      if (err) {
+        console.log(err);
+        return errorRes(res, err)
+      }
+      res.send(html);
+    });
+
+    rows.forEach(product => product.image = path.join("file:///", __dirname, "../public/images", "test.png"));
+    pug.renderFile(path.resolve(__dirname + "/../views/productList.pug"), { products: rows }, (err, html) => {
+      if (err) console.log(err);
+      // pdf.create(html, { format: "A4", localUrlAccess: true }).toFile("Product_List.pdf", (err, result) => {
+      pdf.create(html, { format: "A4", localUrlAccess: true }).toFile(path.resolve(__dirname, "../uploads/pdfs") + "/" + (new Date().toJSON().slice(0, 10) + " Product_List.pdf"), (err, result) => {
+        if (err) return console.log(err);
+        console.log(result);
+      });
+    });
+  });
+}
 
 const addProduct = (req, res) => {
   var { body } = req;
--- index.js
+++ index.js
@@ -9,6 +9,11 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+app.set('views', 'views');
+app.set('view engine', 'pug');
+
+app.use(express.static('public'))
+
 const api_v1 = require("./apis/api_v1.0");
 
 app.use("/api_v1.0", api_v1);
